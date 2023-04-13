500f6eb9cb7470c1f6b1794542f1ae92ba1ab07e
OchirErkhembayar <ochir_erkhembayar@yahoo.com>
2023-04-13 09:54:01 +0100

    modularised adding files and adjusting ticket link

--- main.js
+++ main.js
@@ -10,36 +10,43 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const getTicketInfo = () => {
-    return 'https://trello.com/c/JsjFJv9Z/3-calculate-vehicle-price-v2';
-}
+const createRequestMessage = () => {
+    const ticketLink = fs.readFileSync('./ticket.txt', 'utf8');
+    const files = fs.readFileSync('./files.txt', 'utf8');
 
-const getCode = () => {
-    return fs.readFileSync('./PhpClass.php', 'utf8');
-}
+    let fileString = "";
 
-const ticketData = getTicketInfo();
-const classData = getCode();
+    files.split(/\r?\n/).forEach(line =>  {
+        fileString += `- ${line}`;
+    });
 
-const createRequestMessage = (classData, ticketData) => {
     return `
-    I have the following task ${ticketData}
-    
-    Does this code ${classData} complete this task? Please respond in json format, with the following:
-    isValid: will be true if the task is complete.
-    improvements: will give code improvements and give suggestions for clean code, it should give examples of all things that are wrong or missing.
-    message: will be included if isValid is false, it will explain why the task is not completed. 
-    documentation: will be included if isValid is true. The documentation should simplify the task and code to remind me at a later date what is happening.
-    improved_code: will be included if isValid is true. This should be an improved version of the code.
+    As a code reviewer you are able to review the code in these file links: ${fileString}
+    You should use this trello task link ${ticketLink} and understand the requirements in the content.
+        When reviewing the code in each file link you should ensure that the task requirements have been met. You will need to read all files to ensure you have a full understanding of what has been completed.
+        Things to remember when doing a code review, is to ensure that the code is tested for any scenarios that the task has specified. The code should also be readable where classes have a single responsibility
+        and no unused variables. 
+    Please respond to the code review in json format, with the following:
+        task: it will explain the task to achieve
+        code: it will explain what php code has been found
+        trelloTaskLink: it will show the contents of the trello task
+        numberOfFiles: it will count the number of files that were reviewed
+        filesReviewed: list the files reviewed with the code for each
+        successRate: it will state how confident you are code reviewing this code
+        isValid: will be true if the task is complete.
+        hasTests: does the code contain tests 
+        reviewOverview: give reasons as why you came to the conclusion of your code review
+        message: will be included if isValid is false, it will explain exactly why the task was not completed correctly with reasons for each. 
+        documentation: will be included if isValid is true. The documentation should simplify the task and code to remind me at a later date what is happening.
     `;
 }
 
-const getGptMessage = async (classData, ticketData) => {
+const getGptMessage = async (message) => {
     const completion = await openai.createChatCompletion({
         "model": "gpt-4",
         "messages": [{
             "role": "user", 
-            "content": createRequestMessage(classData, ticketData),
+            "content": message,
         }],
         "temperature": 0.2,
     });
@@ -53,5 +60,5 @@ const getGptMessage = async (classData, ticketData) => {
     });
 }
 
-getGptMessage(classData, ticketData);
+getGptMessage(createRequestMessage());
 
